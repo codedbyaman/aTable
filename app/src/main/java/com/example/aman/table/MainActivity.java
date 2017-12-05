f1cbe672dc4407334b2a02f7362b4e1e2e46f209
amaann <amankumarbostan@gmail.com>
2017-12-05 09:34:59 +0530

    crated dimens for text in all resolutions

--- app/src/main/java/com/example/aman/table/MainActivity.java
+++ app/src/main/java/com/example/aman/table/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
-
+                                    
 
 public class MainActivity extends AppCompatActivity{
     @Override
