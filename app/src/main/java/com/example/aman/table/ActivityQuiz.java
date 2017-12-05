234f30a541e78e1c553cef84b15e847bcf3c082d
amaann <amankumarbostan@gmail.com>
2017-12-05 18:10:53 +0530

    used if condition in ActivityQuiz for answer buttons

--- app/src/main/java/com/example/aman/table/ActivityQuiz.java
+++ app/src/main/java/com/example/aman/table/ActivityQuiz.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import org.w3c.dom.Text;
+
 import java.util.Random;
 
 /**
@@ -20,38 +22,58 @@ public class ActivityQuiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        Button btAns1 = (findViewById(R.id.bt_ans1));
+        Button btAns2 = (findViewById(R.id.bt_ans2));
+        Button btAns3 = (findViewById(R.id.bt_ans3));
+        Button btAns4 = (findViewById(R.id.bt_ans4));
 
         final TextView tvNumber1 = (findViewById(R.id.tv_number1));
+        TextView tvNumber2 = (findViewById(R.id.tv_number2));
+
         Random random = new Random();
         int number1 = random.nextInt(21) + 1;
         tvNumber1.setText("" + number1);
 
-        TextView tvNumber2 = (findViewById(R.id.tv_number2));
         int number2 = random.nextInt(10) + 1;
         tvNumber2.setText("" + number2);
 
+        int number3 = random.nextInt(4) + 1;
 
-        Button btAns1 = (findViewById(R.id.bt_ans1));
-        btAns1.setText("" + number1 * number2);
+        if (number3 == 1) {
+            btAns1.setText(Integer.toString(number1 * number2));
+            btAns2.setText(Integer.toString(number1 * number2 +4));
+            btAns3.setText(Integer.toString(number1 * number2 +6));
+            btAns4.setText(Integer.toString(number1 * number2 +8));
 
-        Button btAns2 = (findViewById(R.id.bt_ans2));
-        btAns2.setText("" + random.nextInt(20) + 1);
-
-        Button btAns3 = (findViewById(R.id.bt_ans3));
-        btAns3.setText("" + random.nextInt(20) + 2);
-
-        Button btAns4 = (findViewById(R.id.bt_ans4));
-        btAns4.setText("" + random.nextInt(20) + 3);
-
-
-
-        btAns1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        } else if (number3 == 2) {
+            btAns2.setText(Integer.toString(number1 * number2));
+            btAns1.setText(Integer.toString(number1 * number2 +4));
+            btAns3.setText(Integer.toString(number1 * number2 +6));
+            btAns4.setText(Integer.toString(number1 * number2 +8));
 
 
-            }
-        });
+        } else if (number3 == 3) {
+            btAns3.setText(Integer.toString(number1 * number2));
+            btAns2.setText(Integer.toString(number1 * number2 +4));
+            btAns1.setText(Integer.toString(number1 * number2 +6));
+            btAns4.setText(Integer.toString(number1 * number2 +8));
 
+
+
+        } else if (number3 == 4) {
+            btAns4.setText(Integer.toString(number1 * number2));
+            btAns2.setText(Integer.toString(number1 * number2 +4));
+            btAns3.setText(Integer.toString(number1 * number2 +6));
+            btAns1.setText(Integer.toString(number1 * number2 +8));}
+
+
+            btAns1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                }
+            });
+
+        }
     }
-}
